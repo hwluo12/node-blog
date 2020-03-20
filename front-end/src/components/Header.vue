a4decd11aace249d81cf7073f6bb69383361df56
hwluo12.shijingshan <hwluo12@163.com>
2020-03-21 01:24:38 +0800

    初始化项目

---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-row class="header">
+    <el-col :span="20">
+      <div>欢迎来到博客</div>
+    </el-col>
+    <el-col :span="4" class="user">
+      <div v-if="login">
+        <i class="el-icon-user"></i>
+        <span class="username">欢迎：{{username}}</span>
+        <i class="el-icon-switch-button"></i>
+      </div>
+      <div v-else>
+        <el-button>登录</el-button>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "Header",
+  computed: {
+    ...mapState(["login", "username"])
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.header {
+  line-height: 60px;
+  border-bottom: 1px solid #eee;
+}
+
+.user {
+  text-align: center;
+}
+
+.username {
+  padding: 0 10px;
+  cursor: pointer;
+}
+
+.login {
+  padding: 0 10px;
+  cursor: pointer;
+}
+</style>
