c1e3983773f676ce4b8efe8b5bb2a6164ae18d5c
hwluo12.shijingshan <hwluo12@163.com>
2020-03-21 22:36:38 +0800

    加密

--- back-end/src/controller/user.js
+++ back-end/src/controller/user.js
@@ -1,7 +1,9 @@
 const { exec, escape } = require('../db/mysql')
+const { genPassword } = require('../utils/cryp')
 
 const login = (username, password) => {
   username = escape(username)
+  password = genPassword(password)
   password = escape(password)
 
   const sql = `select username, realname from users where username=${username} and password=${password}`;
